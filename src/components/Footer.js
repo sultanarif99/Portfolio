e03c0293dfbd43efefb731184437c1c8acc19e94
Sultan Arif <111052090+SAB058@users.noreply.github.com>
2023-11-07 18:00:03 +0500

    Add Constants

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,7 @@ const Footer = () => {
     <div className="footer">
       <p>Sultan Arif</p>
       <p>Full Stack Developer</p>
-      <ul className="socials">
-        {social.map((social) => {
-          return <li key={social.id}>{social.icons}</li>;
-        })}
-      </ul>
+      c
     </div>
   );
 };
